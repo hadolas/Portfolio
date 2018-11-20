2dd8c8eab317c9f3b7d6613976a4c47d80050417
hadolas <hadiyahmughal@gmail.com>
2018-11-20 09:03:36 +0000

    Configure Passport; Create User model; Create Login routes;

--- app.js
+++ app.js
@@ -1,18 +1,34 @@
-var express    = require("express"),
-    app        = express(),
-    bodyParser = require("body-parser"),
-    mongoose   = require("mongoose"),
-    Project    = require("./models/project");
+var express       = require("express"),
+    app           = express(),
+    bodyParser    = require("body-parser"),
+    mongoose      = require("mongoose"),
+    passport      = require("passport"),
+    passportLocal = require("passport-local"),
+    Project       = require("./models/project"),
+    User          = require("./models/user");
     
 var indexRoutes     = require("./routes/index"),
     portfolioRoutes = require("./routes/portfolio");
 
-var databaseUrl = process.env.DATABASEURL
+var databaseUrl = process.env.DATABASE_URL
 mongoose.connect(databaseUrl);
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname+"/public"));
+
+// PASSPORT CONFIGURATION
+var secret = process.env.SECRET;
+app.use(require("express-session")({
+    secret: secret,
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new passportLocal(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.use(indexRoutes);
 app.use(portfolioRoutes);
