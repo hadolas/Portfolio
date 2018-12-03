edb12eafe61c6e314f5008ef1b06e94aa951c1a9
hadolas <hadiyahmughal@gmail.com>
2018-12-03 09:46:41 +0000

    Add scroll effect between sections on index.ejs

---
+++ public/scripts/index.js
@@ -0,0 +1,59 @@
+function smoothScroll(target, duration){
+    var target = document.querySelector(target);
+    // console.log("Target is: "+target);
+    var targetPosition = target.getBoundingClientRect().top + window.scrollY;
+    console.log("Target Position: "+targetPosition);
+    // var startPosition = window.pageYOffset;
+    var startPosition = window.scrollY;
+    console.log("Start Position: "+startPosition);
+    var distance = targetPosition - startPosition;
+    console.log("Distance is: "+distance);
+    console.log("==============");
+    var startTime = null;
+    
+    function animation(currentTime) {
+        if(startTime === null){
+            startTime = currentTime;
+        }
+        var timeElapsed = currentTime - startTime;
+        var run = ease(timeElapsed, startPosition, distance, duration);
+        window.scrollTo(0, run);
+        if(timeElapsed < duration){
+            requestAnimationFrame(animation);
+        }
+    }
+  
+    function ease(t, b, c, d) {
+        t /= d;
+    	return -c * t*(t-2) + b;
+    };
+  
+    requestAnimationFrame(animation);
+}
+
+
+
+var home = document.querySelector("#home-link");
+home.addEventListener("click", function(){
+    smoothScroll("#home", 1500);
+});
+
+var about = document.querySelector("#about-link");
+about.addEventListener("click", function(){
+    smoothScroll("#about", 1500);
+});
+
+var portfolio = document.querySelector("#portfolio-link");
+portfolio.addEventListener("click", function(){
+    smoothScroll("#portfolio", 1500);
+});
+
+var contact = document.querySelector("#contact-link");
+contact.addEventListener("click", function(){
+    smoothScroll("#contact", 1500);
+});
+
+var portfolio2 = document.querySelector("#portfolio-link2");
+portfolio2.addEventListener("click", function(){
+    smoothScroll("#portfolio", 1500);
+});
