2ca588308bba9606493ebdea140b3b038346df6d
hadolas <hadiyahmughal@gmail.com>
2018-12-17 22:44:51 +0000

    Comment script files

--- public/scripts/charCount.js
+++ public/scripts/charCount.js
@@ -1,3 +1,6 @@
+// When writing/editing the summary of a project: if the number of chars is equal to 140, then change the colour of the char-counting-span to red.
+// The char-counting-span is updated at every keyup.
+
 var charsUsed = document.getElementById("charsUsed");
 var summary = document.getElementById("project-summary");
 var maxLength=140;
--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,13 +1,13 @@
 // SMOOTH SCROLL
 function smoothScroll(target, duration){
+    // The target section
     var target = document.querySelector(target);
+    // The Yth pixel of the target section (where the target section begins)
     var targetPosition = target.getBoundingClientRect().top + window.scrollY;
-    // console.log("Target Position: "+targetPosition);
+    // Current postition
     var startPosition = window.scrollY;
-    // console.log("Start Position: "+startPosition);
+    // Total difference between target section and current position
     var distance = targetPosition - startPosition;
-    // console.log("Distance is: "+distance);
-    // console.log("==============");
     var startTime = null;
     
     function animation(currentTime) {
@@ -21,7 +21,8 @@ function smoothScroll(target, duration){
             requestAnimationFrame(animation);
         }
     }
-  
+    
+    // Easing function animation for scroll
     function ease(t, b, c, d) {
         t /= d;
     	return -c * t*(t-2) + b;
@@ -30,10 +31,32 @@ function smoothScroll(target, duration){
     requestAnimationFrame(animation);
 }
 
+// Nav links - when clicked, scroll to corresponding section
+var home = document.querySelector("#home-link");
+home.addEventListener("click", function(){
+    smoothScroll("#home", 1500);
+});
+
+var about = document.querySelector("#about-link");
+about.addEventListener("click", function(){
+    smoothScroll("#about", 1500);
+});
+
+var portfolio = document.querySelector("#portfolio-link");
+portfolio.addEventListener("click", function(){
+    smoothScroll("#portfolio", 1500);
+});
+
+var contact = document.querySelector("#contact-link");
+contact.addEventListener("click", function(){
+    smoothScroll("#contact", 1500);
+});
+
 var portfolio2 = document.querySelector("#portfolio-link2");
 portfolio2.addEventListener("click", function(){
     smoothScroll("#portfolio", 1500);
 });
+
 
 
 // FRONT-END EMAIL VALIDATION
