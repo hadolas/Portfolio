c3f034f012c1a5b98e43164e9787c9f3c3db6e40
hadolas <hadiyahmughal@gmail.com>
2019-12-09 11:59:57 +0000

    Add option to host site on port 3000 if no env var has been provided

--- app.js
+++ app.js
@@ -49,6 +49,6 @@ app.use(function(req, res, next){
 app.use(indexRoutes);
 app.use(portfolioRoutes);
 
-app.listen(process.env.PORT, process.env.IP, function(){
+app.listen(process.env.PORT || 3000, process.env.IP, function(){
     console.log("Portfolio server running");
 });
